8c3550cafcffe122ca1476f01585af96f4be7953
Alberto Castro Fernández <alberto.castrofernandez@swisstph.ch>
2025-01-08 11:18:26 +0100

    Come back to the original approach: discount_years is a period (single value) and not a vector.

--- r_package/healthiar/R/add_monetized_impact.R
+++ r_package/healthiar/R/add_monetized_impact.R
@@ -5,7 +5,7 @@
 #' @param df \code{Data frame} including the column "impact" (health impact)
 #' @param impact \code{Numberic value} referring to the health impacts to be monetized (without attribute function).
 #' @param valuation \code{Numberic value} referring to unit value of a health impact
-#' @param discount_years \code{Numeric value} referring to the period of time to be considered in the discounting.
+#' @param discount_years \code{Numeric value} referring to the period of time (in years) to be considered in the discounting.
 #' @param valuation \code{Numeric value} showing the value of statistical life which will be used in the health impact monetization
 #' @inheritParams include_monetization
 #'
@@ -20,17 +20,6 @@ add_monetized_impact  <- function(df,
                                   discount_years,
                                   discount_shape) {
 
-  discount_years_string <-
-    paste0(first(discount_years), "-",
-           last(discount_years))
-
-  discount_years_length <-
-    length(discount_years)
-
-  # Just in case that the user enter years such as 2021:2024 instead of 1:4
-  discount_years_number <-
-    1:discount_years_length
-
 
   df_with_input <-
     df |>
@@ -38,13 +27,12 @@ add_monetized_impact  <- function(df,
     # Use {{}} to clarify the it refers to the argument and not to the column
     # with the same name
     dplyr::mutate(corrected_discount_rate = {{corrected_discount_rate}},
-                  discount_years_string = discount_years_string,
-                  discount_years_length = discount_years_length,
+                  discount_years = {{discount_years}},
                   discount_shape = {{discount_shape}})
 
   df_with_discount_factor <-
     dplyr::cross_join(x = df_with_input,
-                      y = dplyr::tibble(discount_year = discount_years_number)) |>
+                      y = dplyr::tibble(discount_year = 1 : {{discount_years}})) |>
     # rowwise() because discount_years is a vector
     # otherwise vectors from columns and vectors from discount_years cannot be digested
     # better step by step
@@ -77,7 +65,7 @@ add_monetized_impact  <- function(df,
     dplyr::mutate(
       # Calculate impact after discounting
       impact_before_discount = impact,
-      impact_after_discount = impact / discount_years_length * discount_factor_overtime,
+      impact_after_discount = impact / discount_years * discount_factor_overtime,
       impact = impact_after_discount,
       # Add column for valuation
       valuation = valuation,
