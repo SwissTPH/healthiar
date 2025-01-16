29e3c411195552fef8852956969b4931167469dc
Axel Luyten <axel.luyten@gmail.com>
2025-01-16 12:39:08 +0100

    Added test for `include_social`

--- r_package/healthiar/tests/testthat/test-include_social.R
+++ r_package/healthiar/tests/testthat/test-include_social.R
@@ -21,11 +21,56 @@ testthat::test_that("results correct", {
         social_indicator = data$score,
         n_quantile = 10,
         approach = "quantile"
-        ) |>
+      ) |>
       purrr::pluck("social_main") |>
       dplyr::select(difference_value)  |>
       base::unlist() |>
       base::as.numeric(),
     expect = c(22.52416423, 0.32236823, 14.5680866,0.17252793) # Results on 21 Nov 2024
+  )
+})
+
+testthat::test_that("results from use cases with attribute result and with vectors identical", {
+
+  data <- base::readRDS(testthat::test_path("data", "social_data.rds"))
+
+  bestcost_pm_death <-
+    healthiar::attribute_health(
+      exp_central = as.list(data$PM25_MEAN),
+      cutoff_central = 0,
+      rr_central = 1.08, # The data set contains the RR for the exposure but not per increment. Calculable as e.g. exp(log(1.038017)/(4.848199)*10)
+      erf_shape = "log_linear",
+      erf_increment = 10,
+      bhd_central = as.list(data$MORTALITY_TOTAL),
+      population = data$POPULATION,
+      geo_id_raw = data$CS01012020)
+
+  testthat::expect_equal(
+    object =
+      healthiar::include_social(
+        output = bestcost_pm_death,
+        geo_id_raw = data$CS01012020,
+        social_indicator = data$score,
+        n_quantile = 10,
+        approach = "quantile"
+        ) |>
+      purrr::pluck("social_main") |>
+      dplyr::select(difference_value)  |>
+      base::unlist() |>
+      base::as.numeric(),
+    expect = healthiar::include_social(
+      impact = bestcost_pm_death[["health_main"]]$impact,
+      population = bestcost_pm_death[["health_main"]]$population,
+      bhd = bestcost_pm_death[["health_main"]]$bhd,
+      exp = bestcost_pm_death[["health_main"]]$exp,
+      pop_fraction = bestcost_pm_death[["health_main"]]$pop_fraction,
+      geo_id_raw = data$CS01012020,
+      social_indicator = data$score,
+      n_quantile = 10,
+      approach = "quantile") |>
+      purrr::pluck("social_main") |>
+      dplyr::select(difference_value)  |>
+      base::unlist() |>
+      base::as.numeric()
   )
 })
