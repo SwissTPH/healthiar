1c7f08a205f3f3a0e7e3a33f2945608e53f0fe58
Axel Luyten <axel.luyten@gmail.com>
2025-02-07 12:11:15 +0100

    Re-designed `attribute_health` function help page

--- r_package/healthiar/R/attribute_health.R
+++ r_package/healthiar/R/attribute_health.R
@@ -1,11 +1,49 @@
 #' Attribute health impacts to an environmental stressor
 
-#' @description This function calculates the health impacts, mortality or morbidity,
-#' of exposure to an environmental stressor (air pollution or noise) either using relative or absolute risk.
-#' The health metric inputted to the function is the health metric outputted, e.g. is the baseline health data are mortalities then the result will be mortalities as well.
-
+#' @description This function calculates the health impacts (mortality or morbidity)
+#' of exposure to an environmental stressor (air pollution or noise), using either relative or absolute risk.
+#' @description
+#' The health metric inputted and outputted are the same, e.g. if the baseline health data are mortalities then the result will be mortalities as well. Analogeously for disease cases, DALYs, etc.
 #' @inheritParams attribute
-
+#' @usage
+#' Relative risk case:
+#' attribute_health(
+#'   approach_risk = "relative_risk",
+#'   erf_shape,
+#'   rr_central, rr_lower = NULL, rr_upper = NULL,
+#'   rr_increment,
+#'   exp_central, exp_lower = NULL, exp_upper = NULL
+#'   cutoff_central, cutoff_lower = NULL, cutoff_upper = NULL,
+#'   bhd_central
+#' )
+#'
+#' Absolute risk case:
+#' healthiar::attribute_health(
+#'   approach_risk = "absolute_risk",
+#'   erf_eq_central, erf_eq_lower = NULL, erf_eq_upper = NULL,
+#'   exp_central, exp_lower = NULL, exp_upper = NULL,
+#'   population,
+#'   prop_pop_exp
+#')
+#'
+#' All available arguments:
+#' attribute_health(
+#'   approach_risk = "relative_risk", # Default
+#'   erf_shape = NULL,
+#'   rr_central = NULL, rr_lower = NULL, rr_upper = NULL,
+#'   rr_increment = NULL,
+#'   erf_eq_central = NULL, erf_eq_lower = NULL, erf_eq_upper = NULL,
+#'   exp_central, exp_lower = NULL, exp_upper = NULL,
+#'   prop_pop_exp = 1,
+#'   cutoff_central = NULL, cutoff_lower = NULL, cutoff_upper = NULL,
+#'   bhd_central = NULL, bhd_lower = NULL, bhd_upper = NULL,
+#'   geo_id_disaggregated = NULL,
+#'   geo_id_aggregated = NULL,
+#'   approach_multiexposure = NULL,
+#'   population = NULL,
+#'   info = NULL
+#')
+#'
 #' @return
 #' TBD. This function returns two lists: 1) \code{health_main}, which contains a data frame with the main results and
 #' 2) \code{health_detailed}, which contains detailed (and interim) results.
