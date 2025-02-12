9abd87d72452af787edf9bf818a7b9ec641d7f91
Axel Luyten <axel.luyten@gmail.com>
2025-02-12 15:02:52 +0100

    Updated several function documentations

--- r_package/healthiar/R/attribute_yll_from_lifetable.R
+++ r_package/healthiar/R/attribute_yll_from_lifetable.R
@@ -1,6 +1,6 @@
 #' Attributable years of life lost based on life tables
 #'
-#' @description Calculates the years of life lost attributable to the exposure to an environmental stressor using a life table approach. It provides the central estimate of the impact and the corresponding 95\% confidence intervals (based on the 95\% confidence interval exposure-response function).
+#' @description Calculates the years of life lost attributable to the exposure to an environmental stressor using a life table approach.
 #' @inheritParams attribute
 #' @return
 #' TBD. E.g. This function returns a \code{data.frame} with one row for each value of the
