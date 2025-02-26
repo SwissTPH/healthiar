4a8fea2e50d62457aecb93935dcbe194e093c708
Axel Luyten <117082092+luytax@users.noreply.github.com>
2025-02-26 09:26:25 +0100

    Created function skeleton (code and doc) for `compare_health_new`

---
+++ r_package/healthiar/R/compare_health_new.R
@@ -0,0 +1,25 @@
+#' Compare attributable health cases based on single baseline health value and relative risk
+
+#' @description Calculates the health impacts between two scenarios (e.g. before and after a intervention in a health impact assessments) using either the delta or pif approach.
+#' @details
+#' Note that several input parameters (such as baseline health data and relative risk must be the same to correctly compare the two scenarios.
+#' @param output_attribute_scen_A description
+#' @param output_attribute_scen_B description
+#' @return
+#' TBD
+#' @examples
+#' TBD
+#' @author Alberto Castro & Axel Luyten
+#' @export
+
+compare_health_new <- function(
+    output_attribute_scen_A,
+    output_attribute_scen_B){
+
+
+  # Check that (relevant) input values from scenarios A & B are equal ##########
+
+
+  # Extract input data (for subsequent get_impact call) ########################
+
+}
