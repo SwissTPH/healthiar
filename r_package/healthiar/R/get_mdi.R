ffaf5ee7a60b9dd919f283f07f67e18332ac7c7c
Axel Luyten <117082092+luytax@users.noreply.github.com>
2025-03-10 12:24:13 +0100

    Fixed some warnings

--- r_package/healthiar/R/get_mdi.R
+++ r_package/healthiar/R/get_mdi.R
@@ -51,7 +51,7 @@ get_mdi <- function(
     pop_change,
     no_heating,
     n_quantile
-    ) {
+) {
 
   if (!requireNamespace("ggplot2", quietly = TRUE)) {
     stop("The 'ggplot2' package is required for this function. Please install it if you want to use this function.", call. = FALSE)}
@@ -97,21 +97,25 @@ get_mdi <- function(
   # write.csv(data, "Belgium_MDI_2021.csv", row.names = FALSE)
 
   # Check internal consistency ###################################################
+
   indicators <- c("norm_edu", "norm_unemployed", "norm_single_parent", "norm_pop_change", "norm_no_heating")
 
   # * Descriptive analysis #######################################################
   print("DESCRIPTIVE STATISTICS")
   print(
-  base::sapply(data[c(indicators, "MDI")], function(x)
-    tibble::tibble(MEAN = base::round(base::mean(x), 3), SD = base::round(stats::sd(x), 3), MIN = base::min(x), MAX = base::max(x)))
+    base::sapply(data[c(indicators, "MDI")], function(x)
+      tibble::tibble(MEAN = base::round(base::mean(x), 3), SD = base::round(stats::sd(x), 3), MIN = base::min(x), MAX = base::max(x)))
   )
 
   # * Boxplot ####################################################################
+
   print(
-    ggplot2::ggplot(utils::stack(data[ ,c(indicators, "MDI")]), ggplot2::aes(x = ind, y = values)) + # NOTE: not sure whether it's the stack fct from {utils} or {rlang}
-    ggplot2::geom_boxplot() +
-    ggplot2::theme_minimal() +
-    ggplot2::ggtitle("Boxplot of Normalized Indicators and MDI")
+    ggplot2::ggplot(utils::stack(data[ , c(indicators, "MDI")]), ggplot2::aes(x = ind, y = values)) +
+      ggplot2::geom_boxplot(na.rm = TRUE) +
+      ggplot2::theme_minimal() +
+      ggplot2::ggtitle("Boxplot of Normalized Indicators and MDI") +
+      ggplot2::xlab("Indicator") +
+      ggplot2::ylab("Value")
   )
 
   #ggsave("boxplot.png")
@@ -119,11 +123,14 @@ get_mdi <- function(
   # * Histogram ##################################################################
   print(
     ggplot2::ggplot(data, ggplot2::aes(x = MDI)) +
-    ggplot2::geom_histogram(ggplot2::aes(y = ggplot2::after_stat(density), bins = 30, fill = "blue", alpha = 0.5)) +
-    ggplot2::geom_density(color = "red") +
-    ggplot2::theme_minimal() +
-    ggplot2::ggtitle("Histogram of MDI with Normal Curve")
-)
+      ggplot2::geom_histogram(na.rm = TRUE, ggplot2::aes(y = ggplot2::after_stat(density)), bins = 30, alpha = 0.5) +
+      ggplot2::geom_density(color = "red") +
+      ggplot2::theme_minimal() +
+      ggplot2::theme(legend.position = "none") +
+      ggplot2::scale_x_continuous(limits = c(0, 1), breaks = seq(0, 1, by = 0.2)) +  # x-axis from 0 to 1, with ticks every 0.1
+      ggplot2::ggtitle("Histogram of MDI with Normal Curve") +
+      ggplot2::ylab("Density")
+  )
   #ggsave("MDI_hist.png")
 
   # * Pearson’s correlation coefficient for each indicator #######################
@@ -136,17 +143,17 @@ get_mdi <- function(
   alpha_value <- cronbach_alpha(
     data[, indicators])
 
-  base::print(base::paste("Cronbach's Alpha:", base::round(alpha_value, 3)))
+  base::print(base::paste("CRONBACH'S α:", base::round(alpha_value, 3)))
 
   if ( alpha_value >= 0.9 ) print("Excellent reliability: α ≥ 0.9")
-  if ( alpha_value <= 0.8 & alpha_value <= 0.89 ) print("Good reliability: 0.8 ≤ α ≤ 0.89")
-  if ( alpha_value <= 0.7 & alpha_value <= 0.79 ) print("Acceptable reliability: 0.7 ≤ α ≤ 0.79")
-  if ( alpha_value <= 0.6 & alpha_value <= 0.69 ) print("Questionable reliability: 0.6 ≤ α ≤ 0.69")
+  if ( alpha_value >= 0.8 & alpha_value <= 0.89 ) print("Good reliability: 0.8 ≤ α ≤ 0.89")
+  if ( alpha_value >= 0.7 & alpha_value <= 0.79 ) print("Acceptable reliability: 0.7 ≤ α ≤ 0.79")
+  if ( alpha_value >= 0.6 & alpha_value <= 0.69 ) print("Questionable reliability: 0.6 ≤ α ≤ 0.69")
   if ( alpha_value < 0.6 ) print("Poor reliability: α < 0.6")
 
   return(
     data |>
-           dplyr::relocate(MDI, .after = geo_id_disaggregated) |>
-           dplyr::relocate(MDI_index, .after = MDI)
+      dplyr::relocate(MDI, .after = geo_id_disaggregated) |>
+      dplyr::relocate(MDI_index, .after = MDI)
   )
 }
