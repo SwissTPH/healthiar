d1ade291088cf674f41fdc86b6f4d0c2f7946ac2
Axel Luyten <117082092+luytax@users.noreply.github.com>
2025-04-02 09:19:27 +0200

    Adjusted testing helper fcts

--- r_package/healthiar/tests/testthat/helper.R
+++ r_package/healthiar/tests/testthat/helper.R
@@ -10,7 +10,7 @@ helper_extract_main_results <- function(x){
 helper_extract_detailed_results <- function(x){
   y <- x |>
     purrr::pluck("health_detailed") |>
-    purrr::pluck("raw") |>
+    purrr::pluck("impact_raw") |>
     dplyr::select(impact_rounded)  |>
     base::unlist() |>
     base::as.numeric()
