6fb8b848f179a258d9d9f2b8c8afc344acdc3c71
arnopauwels <105283821+arnopauwels@users.noreply.github.com>
2025-05-05 16:01:05 +0200

    Add PWM option to prepare_exposure()

---
+++ r_package/healthiar/R/prepare_exposure.R
@@ -0,0 +1,68 @@
+#' Prepare exposure data
+
+#' @description
+#' This function prepares tabular population exposure data compatible with the attribute() and compare() functions, based on gridded pollution concentration data and vector data representing geographic units. The function calculates an average concentration value in each geographic unit, weighted by the fraction of the population in each sub-unit.
+#' @param poll_grid \code(SpatRaster) of the pollution concentration data.
+#' @param geo_units \code(sf) of the geographic sub-units.
+#' @param population \code(Vector) containing the total population number in each geographic sub-unit.
+#' @param geo_id_aggregated \code(Vector) containing the id code of the geographic unit the sub-unit belongs to.
+#' @return
+#' This function returns a vector of population exposure values.
+#' @import terra
+#' @import sf
+#' @import dplyr
+#' @export
+#' @author Arno Pauwels
+#' @note Experimental function
+#' @keywords internal
+#' @examples
+#' TBD
+
+prepare_exposure <-
+  function(
+    poll_grid,
+    geo_units,
+    population,
+    geo_id_aggregated
+  ){
+    if (!requireNamespace("terra", quietly = TRUE)) {
+      stop("The 'terra' package is required for this function. Please install it if you want to use this function.", call. = FALSE)}
+
+    ## extract mean concentration in each raw geo unit
+    poll_mean <- terra::extract(poll_grid, geo_units, fun = function(x) {mean(x, na.rm = T)})[, 2]
+    ## create table of non-aggregated exposure for raw output
+    non_aggregated_exposure <- as.data.frame(
+      cbind(
+        geo_id_aggregated,
+        population,
+        poll_mean
+      )
+    )
+    ## create table to calculate pop-weighted exposure
+    exposure <- cbind(
+      geo_id_aggregated,
+      population,
+      poll_mean,
+      geo_units
+    )
+
+    ## calculate population-weighted mean concentration in each aggregated geo unit
+    exposure <- exposure |>
+      group_by(geo_id_aggregated) |>
+      mutate(poll_weighted = population/sum(population)*poll_mean) |>
+      summarise(exposure_value = sum(poll_weighted)) |>
+      mutate(exposure_type = 'Population-weighted mean concentration') |>
+      st_drop_geometry()
+
+    ## build output list
+    main <- as.list(exposure)
+    raw <- as.list(non_aggregated_exposure)
+
+    detailed <- list(raw)
+    names(detailed) <- 'raw'
+
+    output <- list(main, detailed)
+    names(output) <- c('main', 'detailed')
+
+    return(output)
+  }
