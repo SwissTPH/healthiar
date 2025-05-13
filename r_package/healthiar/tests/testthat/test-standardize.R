d8cac935108b3764a01b0ea6d6de32a15dc309cf
Alberto Castro Fernández <alberto.castrofernandez@swisstph.ch>
2025-05-13 11:53:38 +0200

    Make test for compare (deactivated until this feature is avaible)

--- r_package/healthiar/tests/testthat/test-standardize.R
+++ r_package/healthiar/tests/testthat/test-standardize.R
@@ -1,5 +1,5 @@
 # QUANTITATIVE TEST ############################################################
-## USING IMPACT FROM OUTPUT OF ATTRIBUTE ############################################################
+## USING ATTRIBUTE ############################################################
 
 ### ONE GEO UNIT ############################################################
 
@@ -29,7 +29,7 @@ testthat::test_that("results correct |pathway_standardize|single_geo|", {
   testthat::expect_equal(
     object =
       healthiar::standardize(
-        output_healthiar = list(bestcost_pm_mortality_below_40,
+        listed_output_healthiar = list(bestcost_pm_mortality_below_40,
                                 bestcost_pm_mortality_40_plus),
         age_groups = c("below_40", "40_plus"))$health_main$impact_per_100k_inhab,
 
@@ -70,7 +70,7 @@ testthat::test_that("results correct |pathway_standardize|multi_geo|", {
   testthat::expect_equal(
     object =
       healthiar::standardize(
-        output_healthiar = list(bestcost_pm_mortality_below_40_multigeo,
+        listed_output_healthiar = list(bestcost_pm_mortality_below_40_multigeo,
                                 bestcost_pm_mortality_40_plus_multigeo),
         age_groups = c("below_40", "40_plus"))$health_main$impact_per_100k_inhab,
 
@@ -83,7 +83,70 @@ testthat::test_that("results correct |pathway_standardize|multi_geo|", {
 })
 
 
-## USING IMPACT FROM USER ############################################################
+### ONE GEO UNIT ############################################################
+#
+# testthat::test_that("results correct |pathway_standardize|single_geo|", {
+#
+#   bestcost_pm_mortality_below_40_before <-
+#     healthiar::attribute_health(
+#       exp_central = 8.1,
+#       cutoff_central =  0,
+#       bhd_central = 1000,
+#       rr_central = 1.063,
+#       rr_increment = 10,
+#       erf_shape = "log_linear",
+#       population = 1E5)
+#
+#   bestcost_pm_mortality_40_plus_before <-
+#     healthiar::attribute_mod(
+#       output_attribute_1 = bestcost_pm_mortality_below_40_before,
+#       bhd_central = 4000,
+#       exp_central = 10.9,
+#       population = 5E5)
+#
+#
+#   bestcost_pm_mortality_below_40_after <-
+#     healthiar::attribute_mod(
+#       output_attribute_1 = bestcost_pm_mortality_below_40_before,
+#       exp_central = 7.1,
+#       bhd_central = 2000,
+#       population = 2E5)
+#
+#   bestcost_pm_mortality_40_plus_after <-
+#     healthiar::attribute_mod(
+#       output_attribute_1 = bestcost_pm_mortality_40_plus_before,
+#       bhd_central = 8000,
+#       exp_central = 9.9,
+#       population = 1E6)
+#
+#   bestcost_pm_mortality_below_40_compared <-
+#     healthiar::compare(
+#       output_attribute_1 =
+#         healthiar::standardize(
+#           list(bestcost_pm_mortality_below_40_before,
+#                bestcost_pm_mortality_40_plus_before),
+#           age_groups = c("below_40", "40_plus")),
+#       output_attribute_2 =
+#         healthiar::standardize(
+#           list(bestcost_pm_mortality_below_40_after,
+#                bestcost_pm_mortality_40_plus_after),
+#           age_groups = c("below_40", "40_plus")
+#         )
+#       )
+#
+#
+#   testthat::expect_equal(
+#     object =
+#       bestcost_pm_mortality_below_40_compared$health_main$impact_per_100k_inhab,
+#
+#     expected =  (46.20257 - 50.99689)    )
+#     # No study behind.
+#     # Fake numbers to check consistency of result overtime. Results on 2025-01-16
+#
+# })
+
+
+## USING COMPARE ############################################################
 
 # ERROR OR WARNING ########
 ## ERROR #########
