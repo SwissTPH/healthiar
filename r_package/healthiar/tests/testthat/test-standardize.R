d0c94272fd6d00c67b139a994c7ce60c18da56c0
Alberto Castro Fernández <alberto.castrofernandez@swisstph.ch>
2025-05-13 08:45:18 +0200

    Adapt code for multiple geo units (including new test for this)

--- r_package/healthiar/tests/testthat/test-standardize.R
+++ r_package/healthiar/tests/testthat/test-standardize.R
@@ -42,6 +42,47 @@ testthat::test_that("results correct |pathway_standardize|single_geo|", {
   )
 })
 
+### MULTIPLE GEO UNITS ############################################################
+testthat::test_that("results correct |pathway_standardize|multi_geo|", {
+
+  bestcost_pm_mortality_below_40_multigeo <-
+    healthiar::attribute_health(
+      exp_central = list(8.1, 7.1),
+      cutoff_central =  0,
+      bhd_central = list(1000, 2000),
+      rr_central = 1.063,
+      rr_increment = 10,
+      erf_shape = "log_linear",
+      population = list(1E5, 2E5),
+      geo_id_aggregated = c("a", "b"))
+
+  bestcost_pm_mortality_40_plus_multigeo <-
+    healthiar::attribute_mod(
+      output_attribute_1 = bestcost_pm_mortality_below_40,
+      bhd_central = list(4000, 8000),
+      exp_central = list(10.9, 9.9),
+      population = list(5E5, 1E6))
+
+  population_below_40_multigeo <- bestcost_pm_mortality_below_40$health_main$population
+  population_40_plus_multigeo <- bestcost_pm_mortality_40_plus$health_main$population
+  population_total_multigeo <- population_below_40_multigeo + population_40_plus_multigeo
+
+  testthat::expect_equal(
+    object =
+      healthiar::standardize(
+        output_healthiar = list(bestcost_pm_mortality_below_40,
+                                bestcost_pm_mortality_40_plus),
+        age_groups = c("below_40", "40_plus"))$health_main$impact_per_100k_inhab,
+
+    expected =
+      bestcost_pm_mortality_below_40_multigeo$health_main$impact_per_100k_inhab * population_below_40 / population_total_multigeo +
+      bestcost_pm_mortality_40_plus_multigeo$health_main$impact_per_100k_inhab * population_40_plus / population_total_multigeo
+    # No study behind.
+    # Fake numbers to check consistency of result overtime. Results on 2025-01-16
+  )
+})
+
+
 ## USING IMPACT FROM USER ############################################################
 
 # ERROR OR WARNING ########
