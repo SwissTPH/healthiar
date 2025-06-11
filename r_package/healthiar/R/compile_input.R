df99d690d05d426387759a9bd311b1c37069dc95
Alberto Castro Fernández <alberto.castrofernandez@swisstph.ch>
2025-06-11 16:16:04 +0200

    Optimize compile_input() 2: Assign default value for geo_id_dissagregated in attribute_(). Doing this a bug was found and fixed when passing the unused_args. Now attribute_master() has a hidden argument to pass this information from the mask functions.

--- r_package/healthiar/R/compile_input.R
+++ r_package/healthiar/R/compile_input.R
@@ -33,24 +33,17 @@ compile_input <-
     base::list2env(input_args, envir = environment())
 
     # PROCESS GEO ID ###################################################################
-    # If no geo_id_disaggregated is provided (if is NULL) then
-    # single case (only 1 geo unit) is assumed
-    # because an id is always needed to link information
-    if(is.null(geo_id_disaggregated)){
-      geo_id_disaggregated <- base::as.character(1)
-    } else {
-      # geo_ids need to be character because
-      # a) no operations are expected
-      # b) otherwise error somewhere else in the package when mixing character and numeric
-      geo_id_disaggregated <- base::as.character(geo_id_disaggregated)
-    }
+    # geo_ids need to be character because
+    # a) no operations are expected
+    # b) otherwise error somewhere else in the package when mixing character and numeric
+    geo_id_disaggregated <- base::as.character(geo_id_disaggregated)
 
     if(!is.null(geo_id_aggregated)){
       # geo_id_aggegated there is no default value as for geo_id_aggregated
       # so it needs the if statement below.
       # Otherwise, instead of not including the column when NULL
       # no the tibble becomes empty and then error somewhere else
-      geo_id_aggregated <- as.character(geo_id_aggregated)
+      geo_id_aggregated <- base::as.character(geo_id_aggregated)
     }
 
     # PROCESS ERF ######################################################################
