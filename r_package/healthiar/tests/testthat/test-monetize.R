e9badd774471cd33a9c5d9827dc745dfd134ca03
Axel Luyten <117082092+luytax@users.noreply.github.com>
2025-07-21 11:17:50 +0200

    added |pathway_monetization|discount_rate_FALSE|discount_shape_exponential|inflation_FALSE|

--- r_package/healthiar/tests/testthat/test-monetize.R
+++ r_package/healthiar/tests/testthat/test-monetize.R
@@ -30,6 +30,33 @@ testthat::test_that("results correct |pathway_monetization|discount_rate_FALSE|d
   )
 })
 
+testthat::test_that("results correct |pathway_monetization|discount_rate_FALSE|discount_shape_exponential|inflation_FALSE|", {
+
+  testthat::expect_equal(
+    object =
+      healthiar::monetize(
+        impact = 19800,
+        valuation = 541000,
+        discount_shape = "exponential",
+        discount_years = 5
+      )$monetization_main$monetized_impact_rounded,
+    expect =
+      10711800000
+  )
+
+## RESULT(S) COMPARISON ASSESSMENT:
+## 10711.8 million $
+## ASSESSOR:
+## Iracy Pimenta
+## ASSESSMENT DETAILS:
+## Monetization of Ozone reduction policy on premature mortality in China
+## INPUT DATA DETAILS:
+## Example adapted from Chen et al (2015) data to 5 years policy, with no discount rate
+## Paper title: Cost–Benefit Analysis of Reducing Premature Mortality
+## DOI: https://doi.org/10.1007/s11270-015-2316-7
+
+})
+
 ### DISCOUNTING ############################################################
 
 testthat::test_that("results correct |pathway_monetization|discount_rate_TRUE|discount_shape_exponential|inflation_FALSE|", {
