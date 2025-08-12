b413f95d9992a19eab1bf901fd7caadc04d55618
Alberto Castro Fernández <alberto.castrofernandez@swisstph.ch>
2025-08-12 15:05:38 +0200

    Remove deactivated code of variables that are not needed anymore

--- r_package/healthiar/R/get_impact_with_lifetable.R
+++ r_package/healthiar/R/get_impact_with_lifetable.R
@@ -42,13 +42,8 @@ get_impact_with_lifetable <-
     yoa <- input_with_risk_and_pop_fraction |>  dplyr::pull(year_of_analysis) |> dplyr::first()
     yoa_plus_1 <- base::as.numeric(yoa) + 1
 
-    # population_yoa <- base::paste0("population_", yoa)
-    # population_yoa_entry <- base::paste0(population_yoa,"_entry")
-    # population_yoa_plus_1_entry <- base::paste0("population_", yoa_plus_1,"_entry")
-    # population_yoa_end <- base::paste0(population_yoa,"_end")
+    health_outcome <- base::unique(input_with_risk_and_pop_fraction$health_outcome)
 
-    #deaths_yoa <- base::paste0("deaths_", yoa)
-    #impact_yoa <- base::paste0("impact_", yoa)
 
     health_outcome <- base::unique(input_with_risk_and_pop_fraction$health_outcome)
 
