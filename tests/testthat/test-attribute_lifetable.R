c0e31b6c0bb9d85c54aeec2e6885cd15c1a596c4
Alberto Castro Fernández <alberto.castrofernandez@swisstph.ch>
2025-10-28 09:48:03 +0100

    New test for impact_per_100k_inhab_subgroup by sex

--- tests/testthat/test-attribute_lifetable.R
+++ tests/testthat/test-attribute_lifetable.R
@@ -99,6 +99,36 @@ testthat::test_that("results correct |pathway_lifetable|exp_single|exp_time_sing
     expected =
       c(15143.22958, 7925.86465, 22312.77159, 13666.82149, 7157.72614, 20124.28583)/pop*1E5  # Result on 28 Oct 2025
   )
+
+
+  testthat::expect_equal(
+    object =
+      healthiar::attribute_lifetable(
+        health_outcome = "yll",
+        approach_exposure = "single_year",
+        exp_central = data_mort$exp[2], #exp CH 2019
+        prop_pop_exp = 1,
+        cutoff_central = data_mort$cutoff[2], # WHO AQG 2021
+        rr_central = data_mort[2,"rr_central"],
+        rr_lower = data_mort[2,"rr_lower"],
+        rr_upper = data_mort[2,"rr_upper"],
+        rr_increment = 10,
+        erf_shape = "log_linear",
+        age_group = c(data_lifetable[["male"]]$age,
+                      data_lifetable[["female"]]$age),
+        sex = base::rep(c("male", "female"), each = 100),
+        population = c(data_lifetable[["male"]]$population,
+                       data_lifetable[["female"]]$population),
+        bhd_central = c(data[["pop"]]$number_of_deaths_male,
+                        data[["pop"]]$number_of_deaths_female),
+        year_of_analysis = 2019,
+        info = data_mort$pollutant[2],
+        min_age = if(is.na(data_mort$min_age[2])) NULL else data_mort$min_age[2]
+      )$health_detailed$results_by_sex$impact_per_100k_inhab_subgroup, # impact_per_100k_inhab_subgroup
+    expected = c(
+      c(15143.22958, 7925.86465, 22312.77159) / sum(data_lifetable[["male"]]$population),
+      c(13666.82149, 7157.72614, 20124.28583) / sum(data_lifetable[["female"]]$population)) * 1E5  # Result on 28 Oct 2025
+  )
 })
 
 testthat::test_that("results correct |pathway_lifetable|exp_single|exp_time_single_year|newborns_FALSE|min_age_TRUE|max_age_FALSE|time_horizon_TRUE|iteration_FALSE|", {
