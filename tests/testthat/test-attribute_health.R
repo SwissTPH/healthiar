3f984d618423adcdf1eef578c6d632dc821d0946
Gianni Ardielli <gianni.ardielli@swisstph.ch>
2025-11-19 16:22:54 +0100

    added exotic test |pathway_rr|erf_function|exp_dist|cutoff_TRUE|iteration_FALSE|strat_TRUE|yld_FALSE|uncertainty_TRUE|

--- tests/testthat/test-attribute_health.R
+++ tests/testthat/test-attribute_health.R
@@ -1619,6 +1619,92 @@ testthat::test_that("results the same |pathway_rr|erf_lin_lin|exp_dist|iteration
 ## INPUT DATA DETAILS: obtained from the study mentioned. Calculated attributable deaths from pm2.5 in 2020.
 
 
+##### Stratification (sex/age) ####################################################################
+
+  testthat::test_that("results the same |pathway_rr|erf_function|exp_dist|cutoff_TRUE|iteration_FALSE|strat_TRUE|yld_FALSE|uncertainty_TRUE|",{
+    data <- base::readRDS(testthat::test_path("data", "LMU_O3_COPD_mort_2016.rds"))
+    #Exotic test based on real data but does produce real world results
+
+
+    #percentage of variation
+    exp_change <-1.1
+    cutoff_change <-0.8
+    bhd_change <-0.9
+    rr_change = bhd_change <-1.2
+    uncert_factor <- 20#set uncertainty factor
+    # set central values and variate by percentage
+    exp_c <- base::signif(unlist(lapply(data$Mean.O3, function(x) x * exp_change^(0:3))),5)
+    cutoff_c <- rep(base::signif(unlist(lapply(1, function(x)  x * cutoff_change^(0:3))),5),times = length(data$Mean.O3))
+    bhd_c <- rep(base::signif(unlist(lapply(29908, function(x) x * bhd_change^(0:3))),5),times = length(data$Mean.O3))
+    exp_c
+    x <-healthiar::attribute_health(
+      approach_risk = "relative_risk",
+      age = rep(c("below_50", "below_50", "50_plus", "70_plus"),times = length(data$Mean.O3)),
+      sex = rep(c("male", "female", "male", "female"),times = length(data$Mean.O3)),
+      exp_central = exp_c,
+      exp_lower = exp_c - exp_c/uncert_factor,
+      exp_upper = exp_c + exp_c/uncert_factor,
+      cutoff_central = cutoff_c,
+      cutoff_lower = cutoff_c - cutoff_c/uncert_factor,
+      cutoff_upper = cutoff_c + cutoff_c/uncert_factor,
+      bhd_central = bhd_c,
+      bhd_lower = bhd_c - bhd_c/uncert_factor,
+      bhd_upper = bhd_c + bhd_c/uncert_factor,
+      erf_eq_central = splinefun(data$x, data$y, method="natural"),
+      erf_eq_lower  = splinefun(data$x, data$y_l, method="natural"),
+      erf_eq_upper  = splinefun(data$x, data$y_u, method="natural"),
+      prop_pop_exp = rep(data$Population.affected, each = 4)
+      )
+    x$health_detailed$results_by_age_group$impact_rounded
+    testthat::expect_equal(
+      ## test if age group results are correct
+      object =x$health_detailed$results_by_age_group$impact_rounded,
+      expected = c(
+        729,555,882,729,555,883,728,554,881,692,527,838,693,527,838,692,527,837,
+        765,582,926,766,583,927,765,582,925,670,510,811,671,511,812,669,509,810,
+        637,485,771,637,485,771,636,484,770,704,536,852,705,536,853,703,535,851,
+        775,590,937,775,590,938,774,589,937,736,560,890,736,560,891,735,560,890,
+        813,619,984,814,619,985,813,619,983,555,422,671,555,422,671,555,422,671,
+        527,401,638,527,401,638,527,401,638,583,443,705,583,444,705,582,443,705,
+        530,404,642,531,404,642,530,404,642,504,384,610,504,384,610,504,383,610,
+        557,424,674,557,424,674,557,424,674,577,439,698,577,439,698,576,439,697,
+        548,417,663,548,417,663,548,417,662,605,461,732,606,461,733,605,461,732,
+        717,546,867,717,546,867,717,546,867,681,518,824,681,519,824,681,518,824,
+        753,573,910,753,573,911,753,573,910,690,525,835,690,525,835,690,525,835,
+        655,499,793,656,499,793,655,499,793,724,552,876,725,552,877,724,551,876,
+        743,565,898,743,566,899,743,565,898,706,537,853,706,537,854,705,537,853,
+        780,594,943,780,594,943,780,594,943
+      )
+    )
+
+    x$health_detailed$results_by_sex$impact_rounded
+    testthat::expect_equal(
+      ## test if sex results are correct
+      object =x$health_detailed$results_by_sex$impact_rounded,
+      expected = c(
+        862,656,1043,862,656,1043,861,655,1042,
+        819,623,990,819,623,991,818,623,990,
+        905,689,1095,905,689,1095,904,688,1094,
+        805,612,974,805,613,974,804,612,973,
+        764,582,925,765,582,926,764,581,924,
+        845,643,1022,846,644,1023,844,642,1022,
+        908,691,1098,908,691,1099,907,691,1098,
+        862,656,1043,863,657,1044,862,656,1043,
+        953,726,1153,954,726,1154,953,725,1153,
+        1139,867,1378,1139,867,1378,1138,867,1377,
+        1082,824,1309,1082,824,1309,1081,823,1308,
+        1196,910,1447,1196,910,1447,1195,910,1446,
+        1086,827,1314,1086,827,1314,1086,826,1314,
+        1032,785,1248,1032,786,1249,1031,785,1248,
+        1140,868,1380,1141,868,1380,1140,868,1379,
+        1186,903,1435,1186,903,1435,1186,903,1434,
+        1127,858,1363,1127,858,1363,1126,858,1363,
+        1245,948,1507,1246,948,1507,1245,948,1506
+      )
+    )
+  })
+
+
 
 #### ITERATION ##################################################################
 testthat::test_that("results the same no cutoff |pathway_rr|erf_log_lin|exp_dist|iteration_TRUE|strat_FALSE|yld_FALSE|uncertainty_FALSE|", {
