cc102a6ef051ea8c72968e24bb6d26c59278bdfc
Alberto Castro Fernández <alberto.castrofernandez@swisstph.ch>
2026-01-16 15:30:27 +0100

    Add test in standardize() without ref_prop_pop

--- tests/testthat/test-standardize.R
+++ tests/testthat/test-standardize.R
@@ -46,9 +46,47 @@ testthat::test_that("results correct |pathway_standardize|single_geo|", {
       # No study behind.
       # Fake numbers to check consistency of result overtime. Results on 2025-01-16
   )
+
 })
 
+#### WITHOUT REF_PROP_POP ############################################################
+
+testthat::test_that("results the same |pathway_standardize|single_geo|", {
+
+  bestcost_pm_mortality <-
+    healthiar::attribute_health(
+      age_group = c("below_40", "above_40"),
+      exp_central = c(8.1, 10.9),
+      cutoff_central =  0,
+      bhd_central = c(1000, 4000),
+      rr_central = 1.063,
+      rr_increment = 10,
+      erf_shape = "log_linear",
+      population = c(1E5, 5E5))
+
+  testthat::expect_equal(
+    object =
+      healthiar::standardize(
+        output_attribute = bestcost_pm_mortality,
+        age_group = c("below_40", "above_40")
+        #,
+        #ref_prop_pop = c(0.5, 0.5) # Deactivating ref_prop_pop
+      )$health_main$impact_per_100k_inhab,
+
+    expected =  base::sum(
+      bestcost_pm_mortality_below_40$health_main$impact_per_100k_inhab * 0.1666667,
+      bestcost_pm_mortality_40_plus$health_main$impact_per_100k_inhab * 0.8333333)
+    # No study behind.
+    # Fake numbers to check consistency of result overtime. Results on 2025-01-16
+  )
+
+
+})
+
+
 ### MULTIPLE GEO UNITS ############################################################
+
+#### WITHOUT REF_PROP_POP ############################################################
 testthat::test_that("results correct |pathway_standardize|multi_geo|", {
 
   bestcost_pm_mortality_multigeo <-
@@ -96,6 +134,54 @@ testthat::test_that("results correct |pathway_standardize|multi_geo|", {
   )
 })
 
+#### WITHOUT REF_PROP_POP ############################################################
+testthat::test_that("results correct |pathway_standardize|multi_geo|", {
+
+  bestcost_pm_mortality_multigeo <-
+    healthiar::attribute_health(
+      geo_id_micro = c("a", "a", "b", "b"),
+      age_group = c("below_40", "above_40", "below_40", "above_40"),
+      exp_central = c(8.1, 10.9, 7.1, 9.9),
+      cutoff_central =  0,
+      bhd_central = c(1000, 4000, 2000, 8000),
+      rr_central = 1.063,
+      rr_increment = 10,
+      erf_shape = "log_linear",
+      population = c(1E5, 5E5, 2E5, 1E6))
+
+  bestcost_pm_mortality_below_40_multigeo <-
+    healthiar::attribute_health(
+      exp_central = c(8.1, 7.1),
+      cutoff_central =  0,
+      bhd_central = c(1000, 2000),
+      rr_central = 1.063,
+      rr_increment = 10,
+      erf_shape = "log_linear",
+      population = c(1E5, 2E5),
+      geo_id_micro = c("a", "b"))
+
+  bestcost_pm_mortality_40_plus_multigeo <-
+    healthiar::attribute_mod(
+      output_attribute = bestcost_pm_mortality_below_40_multigeo,
+      bhd_central = c(4000, 8000),
+      exp_central = c(10.9, 9.9),
+      population = c(5E5, 1E6))
+
+  testthat::expect_equal(
+    object =
+      healthiar::standardize(
+        output_attribute = bestcost_pm_mortality_multigeo,
+        age_group = c("below_40", "above_40"),
+        #ref_prop_pop = c(0.5, 0.5)
+        )$health_main$impact_per_100k_inhab,
+
+    expected =
+      bestcost_pm_mortality_below_40_multigeo$health_main$impact_per_100k_inhab * 0.1666667 +
+      bestcost_pm_mortality_40_plus_multigeo$health_main$impact_per_100k_inhab * 0.8333333
+    # No study behind.
+    # Fake numbers to check consistency of result overtime. Results on 2025-01-16
+  )
+})
 
 ### ONE GEO UNIT ############################################################
 #
