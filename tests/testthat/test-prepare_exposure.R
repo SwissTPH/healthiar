c1ea4aef3936e8982580d825a7bf2ec6ac121cce
Arno Pauwels <arno.pauwels@sciensano.be>
2026-02-18 11:46:43 +0100

    New tests added

--- tests/testthat/test-prepare_exposure.R
+++ tests/testthat/test-prepare_exposure.R
@@ -11,7 +11,24 @@ testthat::test_that("results correct", {
         geo_units = municip,
         population = sf::st_drop_geometry(municip$population),
         geo_id_macro = sf::st_drop_geometry(municip$region)
-      )$main$exp_value,
+      )$exposure_main$exposure_mean,
+    expect = results$exposure
+  )
+})
+
+testthat::test_that("results correct", {
+
+  municip <- sf::st_read(testthat::test_path("data", "municipalities_brussels.gpkg"), quiet = TRUE)
+  pm25 <- terra::rast(testthat::test_path("data", "pm25.tif"))
+  results <- utils::read.csv(testthat::test_path("data", "exp_simple_results.csv"))
+
+  testthat::expect_equal(
+    object =
+      healthiar::prepare_exposure(
+        poll_grid = pm25,
+        geo_units = municip,
+        geo_id_micro = sf::st_drop_geometry(municip$name)
+      )$exposure_main$exposure_mean,
     expect = results$exposure
   )
 })
