01c77f40137a45f027ad1a1b7989686cd4a22699
Axel Luyten <117082092+luytax@users.noreply.github.com>
2026-02-19 15:21:54 +0100

    extract & collect code to create exdat_cantons.rda

--- data-raw/create_data_for_package.R
+++ data-raw/create_data_for_package.R
@@ -11,60 +11,14 @@ library(dplyr)
 
 ## Load data ####
 
-load("C:/Users/luytax/switchdrive/Hitze/HiMoMo/2025_himomo2024/data/clean/population_1969_2024_canton.RData")
-population_1969_2024_canton <- population_1969_2024_canton |>
-  dplyr::filter(year == 2023) |>
-  dplyr::mutate(CH = rowSums(dplyr::across(AG:ZH)))
 
-population_1969_2024_canton <- tidyr::pivot_longer(
-  data = population_1969_2024_canton,
-  cols = 2:ncol(population_1969_2024_canton),
-  names_to = "canton",
-  values_to = "population"
-)
 
 load("C:/Users/luytax/switchdrive/Hitze/HiMoMo/2025_himomo2024/data/clean/canton_key.RData")
-load("C:/Users/luytax/switchdrive/Other projects/2025_CHair/chair/data/clean/data_20_plus.Rdata")
 
 ## Prepare datasets ####
 
 ## RR
-pm_lc <- read.csv("varia/Workshops_and_demos/2025_09_30_international_workshop/_115a_rz.csv")
-pm_lc <- pm_lc |>
-  ## Rename columns
-  dplyr::select(
-    year,
-    canton = region_name,
-    lung_cancer_incidence = n,
-    geo
-  ) |>
-  ## Add data
-  dplyr::filter(year == 2023) |>
-  dplyr::filter(geo == "kt") |>
-  dplyr::select(-geo) |>
-  dplyr::mutate(exposure = dplyr::if_else(
-    year == 2023,
-    true = data_20_plus |> dplyr::filter(year == 2023) |> dplyr::pull(pm2.5),
-    false = 0)
-  ) |>
-  dplyr::mutate(exposure_type = "population_weighted_mean") |>
-  dplyr::left_join(
-    x = _,
-    y = population_1969_2024_canton,
-    by = dplyr::join_by(year, canton)
-  ) |>
-  dplyr::arrange(year, canton) |>
-  dplyr::mutate(
-    rr = 1.16,
-    rr_l = 1.10,
-    rr_u = 1.23,
-    increment = 10,
-    function_shape = "log_linear"
-  ) |>
-  dplyr::mutate(cutoff = 5) |>
-  dplyr::mutate(
-    pollutant = "PM2.5", .after = exposure
-  )
+
 
 ## AR
 noise_ha <- pm_lc |>
@@ -121,47 +75,7 @@ pm_lc_ch <- pm_lc |>
   dplyr::select(-canton)
 
 # pm_lc_cantons <- pm_lc |>
-exdat_cantons <- pm_lc |>
-  dplyr::filter(canton != "CH") |>
-  dplyr::filter(year == 2023) |>
-  mutate(
-    language_main = case_when(
-      canton %in% c("ZH", "BE", "LU", "UR", "SZ", "OW", "NW", "GR", "GL", "ZG", "SO", "BS", "BL", "SH", "AR", "AI", "SG", "GR",
-                    "AG", "TG") ~ "German",
-      canton %in% c("FR", "VD", "NE", "JU", "GE", "VS") ~ "French",
-      canton %in% c("TI") ~ "Italian",
-      TRUE ~ NA_character_
-    ),
-    canton_long = case_when(
-      canton == "ZH" ~ "Zurich",
-      canton == "BE" ~ "Bern",
-      canton == "LU" ~ "Lucerne",
-      canton == "UR" ~ "Uri",
-      canton == "SZ" ~ "Schwyz",
-      canton == "OW" ~ "Obwalden",
-      canton == "NW" ~ "Nidwalden",
-      canton == "GL" ~ "Glarus",
-      canton == "ZG" ~ "Zug",
-      canton == "FR" ~ "Fribourg",
-      canton == "SO" ~ "Solothurn",
-      canton == "BS" ~ "Basel-Stadt",
-      canton == "BL" ~ "Basel-Landschaft",
-      canton == "SH" ~ "Schaffhausen",
-      canton == "AR" ~ "Appenzell Ausserrhoden",
-      canton == "AI" ~ "Appenzell Innerrhoden",
-      canton == "SG" ~ "St. Gallen",
-      canton == "GR" ~ "Grisons",
-      canton == "AG" ~ "Aargau",
-      canton == "TG" ~ "Thurgau",
-      canton == "TI" ~ "Ticino",
-      canton == "VD" ~ "Vaud",
-      canton == "VS" ~ "Valais",
-      canton == "NE" ~ "Neuchâtel",
-      canton == "GE" ~ "Geneva",
-      canton == "JU" ~ "Jura",
-      TRUE ~ NA_character_
-    )
-  )
+
 
 noise_ha_ch <- noise_ha |>
   dplyr::filter(canton == "CH") |>
@@ -266,3 +180,4 @@ save(exdat_pm, file = "data/exdat_pm.rda")
 #   geo_id_macro = sf::st_drop_geometry(exdat_pwm_2$region)
 # )
 
+usethis::use_data(DATASET, overwrite = TRUE)
