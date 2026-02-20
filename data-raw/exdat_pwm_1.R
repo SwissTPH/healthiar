531d3b19b276bc6f4b3a47c3e8c9925cde1a6784
Axel Luyten <117082092+luytax@users.noreply.github.com>
2026-02-20 10:46:24 +0100

    create exdat_pwm_1

---
+++ data-raw/exdat_pwm_1.R
@@ -0,0 +1,8 @@
+# exdat_pwm_2 ##################################################################
+
+## NOTE AL 2026-02-20: because this dataset (.tif) can't be saved as .rda
+## without losing some info, it is saved in inst/extdata
+
+## NOTE AL 2026-02-20: the original data set was called
+## pm25.tif; renamed to
+## exdat_pwm_2_pm25.tif
