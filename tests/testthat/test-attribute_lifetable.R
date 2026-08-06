5cf46fee0ec76ce032d813d4e34b4d728e6c01c8
ungatoverde <alberto.acf@gmail.com>
2026-08-06 19:18:25 +0200

    New tests to ensure that population and bhd between 0 and 1 are accepted

--- tests/testthat/test-attribute_lifetable.R
+++ tests/testthat/test-attribute_lifetable.R
@@ -101,8 +101,55 @@ testthat::test_that("results correct |pathway_lifetable|exp_single|exp_time_sing
     expected = c(
       c(15143.22958, 7925.86465, 22312.77159) / sum(data_lifetable[["male"]]$population),
       c(13666.82149, 7157.72614, 20124.28583) / sum(data_lifetable[["female"]]$population)) * 1E5  # Result on 28 Oct 2025
+  )  
+})
+
+testthat::test_that("results the same |pathway_lifetable|exp_single|exp_time_single_year|newborns_FALSE|min_age_TRUE|max_age_FALSE|time_horizon_TRUE|iteration_FALSE|", {
+
+  data <- base::readRDS(testthat::test_path("data", "airqplus_pm_deaths_yll.rds"))
+  data_mort <- base::readRDS(testthat::test_path("data", "input_data_mortality.rds"))
+  data_lifetable <- base::readRDS(testthat::test_path("data", "lifetable_withPopulation.rds"))
+
+  # Check that decimals between 0 and 1 are allowed in bhd and population
+  population <- 
+    c(data_lifetable[["male"]]$population,
+      data_lifetable[["female"]]$population)
+  population[100] <- 0.5 
+
+  bhd_central <- 
+    c(data[["pop"]]$number_of_deaths_male,
+      data[["pop"]]$number_of_deaths_female)
+  bhd_central[100] <- 0.3  
+    
+
+    testthat::expect_equal(
+    object =
+      healthiar::attribute_lifetable(
+        health_outcome = "yll",
+        approach_exposure = "single_year",
+        exp_central = data_mort$exp[2], #exp CH 2019
+        prop_pop_exp = 1,
+        cutoff_central = data_mort$cutoff[2], # WHO AQG 2021
+        rr_central = data_mort[2,"rr_central"],
+        rr_lower = data_mort[2,"rr_lower"],
+        rr_upper = data_mort[2,"rr_upper"],
+        rr_increment = 10,
+        erf_shape = "log_linear",
+        age_group = c(data_lifetable[["male"]]$age,
+                      data_lifetable[["female"]]$age),
+        sex = base::rep(c("male", "female"), each = 100),
+        population = population,
+        bhd_central = bhd_central,
+        year_of_analysis = 2019,
+        info = data_mort$pollutant[2],
+        min_age = if(is.na(data_mort$min_age[2])) NULL else data_mort$min_age[2]
+      )$health_main$impact,
+    expected =
+      c(28814.1063, 15085.5567, 42443.4722) # Result on 06 August 2026
   )
 })
+
+
 
 testthat::test_that("results correct |pathway_lifetable|exp_single|exp_time_single_year|newborns_FALSE|min_age_TRUE|max_age_FALSE|time_horizon_TRUE|iteration_FALSE|", {
 
