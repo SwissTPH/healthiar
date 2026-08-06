de1af65d3371fd58dad65578f46868727f7686cf
ungatoverde <alberto.acf@gmail.com>
2026-08-06 19:14:31 +0200

    error if not positive and error if negative

--- R/validate_input_attribute.R
+++ R/validate_input_attribute.R
@@ -334,19 +334,39 @@ validate_input_attribute <-
 
     if(is_lifetable){
 
-      ### error_if_below_1 #####
+      ### error_if_not_positive #####
 
-      # Find the arguments with values <1
-      args_value_below_1 <-
-        input_args_value[lifetable_args_with_values_1_or_above] |>
-        purrr::keep(.p = ~ base::any(.x < 1)) |>
+      # No life table arguments currently require values > 0 at validation stage
+      args_value_not_positive <-
+        input_args_value[lifetable_args_with_values_above_0] |>
+        purrr::keep(.p = ~ base::any(.x <= 0)) |>
         base::names()
 
 
-      if(base::length(args_value_below_1) > 0) {
+      if(base::length(args_value_not_positive) > 0) {
         base::stop(
-          base::paste0("The values in the following arguments must be 1 or higher: ",
-                       base::toString(args_value_below_1),
+          base::paste0("The values in the following arguments must not be lower than 0: ",
+                       base::toString(args_value_not_positive),
+                       "."),
+          call. = FALSE
+        )
+
+      }
+
+      ### error_if_negative #####
+
+      # Population and baseline health data may be 0 but not negative in life
+      # table calculations; structural zero-population cases are handled later
+      args_value_below_0_lifetable <-
+        input_args_value[lifetable_args_with_values_0_or_above] |>
+        purrr::keep(.p = ~ base::any(.x < 0)) |>
+        base::names()
+
+
+      if(base::length(args_value_below_0_lifetable) > 0) {
+        base::stop(
+          base::paste0("The values in the following arguments must be 0 or higher: ",
+                       base::toString(args_value_below_0_lifetable),
                        "."),
           call. = FALSE
         )
